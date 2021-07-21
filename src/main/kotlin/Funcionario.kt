af94846deafb55eae9afc55e1fc6de4fa6d75fbb
Lucas de Almeida Salles <lucassalles@drrr.us>
2021-07-21 15:56:18 -0300

    ByteBank Project

---
+++ src/main/kotlin/Funcionario.kt
@@ -0,0 +1,12 @@
+class Funcionario(
+    val nome: String,
+    val cpf: String,
+    val salario: Double,
+) {
+    fun bonificacao():Double {
+        return salario * 0.1
+    }
+
+
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,33 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    testaComportamentosConta()
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "123.123.132-20",
+        salario = 1000.0,
+    )
+
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salario ${alex.salario}")
+    println("bonificacao ${alex.bonificacao()}")
+    println()
+
+    val lucas = Gerente(
+        nome = "Lucas",
+        cpf = "123.123.123-12",
+        salario = 2000.0,
+        senha = 1234
+    )
+
+    println("nome ${lucas.nome}")
+    println("cpf ${lucas.cpf}")
+    println("salario ${lucas.salario}")
+    println("bonificacao ${lucas.bonificacao()}")
+    if (lucas.autenticacao(1234)){
+        println("Autenticação com Sucesso!")
+    }else
+        println("Falha na Autenticação!")
 }
 
 
