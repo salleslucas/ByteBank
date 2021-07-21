23accef6bbee223c9f17444f0d03859055ce0b27
Lucas de Almeida Salles <lucassalles@drrr.us>
2021-07-21 14:40:30 -0300

    ByteBank Project

---
+++ src/main/kotlin/Conta.kt
@@ -0,0 +1,35 @@
+class Conta(
+    var titular: String,
+    val numero: Int
+) {
+    var saldo = 0.0
+        private set
+
+    //    ****   Construtor Secundário ****
+//    constructor(titular: String, numero: Int){
+//        this.titular = titular
+//        this.numero = numero
+//    }
+
+
+    fun deposita(valor: Double) {
+        if (valor > 0) {
+            this.saldo += valor
+        }
+    }
+
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            saldo -= valor
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.deposita(valor)
+            return true
+        }
+        return false
+    }
+}
